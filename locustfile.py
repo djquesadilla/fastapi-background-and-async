d46a7fc2df30ff160dd446110345d501d751447a
Fábio <fabio@impossible.com>
2023-10-18 20:24:50 +0100

    Experimenting with asyncs and background tasks. Added Locust for stress testing

---
+++ locustfile.py
@@ -0,0 +1,6 @@
+from locust import HttpUser, task
+
+class SyntheticUsersBackend(HttpUser):
+    @task
+    def write_files_in_background(self):
+        self.client.post("/write-file/")
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from typing import Union
+import os
+import time
 
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
+def write_a_new_file():
+    # count number of files with txt extension in tmp directory and add 1
+    file_count = len([name for name in os.listdir("tmp") if name.endswith(".txt")]) + 1
+    # write a new file
+    with open(f"tmp/log_{file_count}.txt", mode="w") as file:
+        content = f"counting files to test this API thing: {file_count}"
+        file.write(content)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # sleep for 10 seconds
+    print(f"PID: {os.getpid()} -- sleeping for 10 seconds")
+    time.sleep(10)
+
+@app.post("/write-file")
+async def write_file(background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_a_new_file)
+    return {"message": "Writing file in background"}
